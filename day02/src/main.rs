2b6a2914539c59775eebaae32fa4a93d8bc2b663
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-03-23 13:34:53 +0100

    feat: solve day 2

---
+++ day02/src/main.rs
@@ -0,0 +1,25 @@
+fn price(quality: u64) -> u64 {
+    quality.pow(3) * 69 + 510
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    let numbers = input.split_once("\n\n").unwrap().1
+    .lines().map(|line| line.parse::<u64>().unwrap()).collect::<Vec<_>>();
+
+    let part1 = price(numbers[numbers.len() / 2]);
+    println!("{part1}");
+
+    let part2 = price(numbers
+        .iter()
+        .filter(|&n| n % 2 == 0)
+        .sum());
+    println!("{part2}");
+
+    let part3 = numbers
+        .iter()
+        .filter(|&&n| price(n) < 15_000_000_000_000)
+        .max()
+        .unwrap();
+    println!("{part3}");
+}
